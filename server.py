3edc4459fac42529b167900c2cd9ea3cff8657e0
Hannah He <hannahhe18@gmail.com>
2018-12-07 20:40:33 -0500

    fixed bug where latexcode wasn't defined in server

--- server.py
+++ server.py
@@ -24,25 +24,25 @@ def graph(): #files are different from other things: if you wanted to send the f
         graph1 = Function()
         func = request.form["Function"]
         # Here is the code to convert the post request to an OpenCV object via https://gist.github.com/mjul/32d697b734e7e9171cdb
-        if request.method == 'PUT' and 'GraphImage' in request.files:
-            photo = request.files['GraphImage']
-            in_memory_file = io.BytesIO()
-            photo.save(in_memory_file)
-            data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
-            color_image_flag = 1
-            img = cv2.imdecode(data, color_image_flag)
-        if(isinstance(graph1, GraphTheory)):
-            laTexCode = graph1.detectPointsAndAxes(img, numEdges)
-        if(isinstance(graph1, ScatterPlot)):
-            laTexCode = graph1.detectPointsAndAxes(img)
-        if(isinstance(graph1, Function)):
-            D1 =  request.form["D1"]
-            D2 =  request.form["D2"]
-            X1 =  request.form["X1"]
-            X2 =  request.form["X2"]
-            Y1 =  request.form["Y1"]
-            Y2 =  request.form["Y2"]
-            laTexCode = graph1.plotFunction(func, D1, D2, X1, X2, Y1, Y2)
+    if request.method == 'PUT' and 'GraphImage' in request.files:
+        photo = request.files['GraphImage']
+        in_memory_file = io.BytesIO()
+        photo.save(in_memory_file)
+        data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+        color_image_flag = 1
+        img = cv2.imdecode(data, color_image_flag)
+    if(isinstance(graph1, GraphTheory)):
+        laTexCode = graph1.detectPointsAndAxes(img, numEdges)
+    if(isinstance(graph1, ScatterPlot)):
+        laTexCode = graph1.detectPointsAndAxes(img)
+    if(isinstance(graph1, Function)):
+        D1 =  request.form["D1"]
+        D2 =  request.form["D2"]
+        X1 =  request.form["X1"]
+        X2 =  request.form["X2"]
+        Y1 =  request.form["Y1"]
+        Y2 =  request.form["Y2"]
+        laTexCode = graph1.plotFunction(func, D1, D2, X1, X2, Y1, Y2)
 
 
     return jsonify({'latex': laTexCode}) #jsonify: turns python dictionaries into json
